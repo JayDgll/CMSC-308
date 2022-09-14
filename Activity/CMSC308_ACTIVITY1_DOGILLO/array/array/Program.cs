baff0bdc9240e6fba6b451c0a339d899d2d5bdcb
Jay Dogillo <104479630+JayDgll@users.noreply.github.com>
2022-09-14 12:10:42 +0800

    Add files via upload

---
+++ Activity/CMSC308_ACTIVITY1_DOGILLO/array/array/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace array;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string[] email = { "jay.dogillo@gmail.com", "louiejerome.roldan@gmail.com", "leo.po@gmail.com" };
+        string[] username = { "jay", "louie jerome", "leo po" };
+        string[] password = { "jay1234", "louiejerome1234", "leopo1234" };
+
+        int index;
+        Console.Write("Index: ");
+        index = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Email: " + email[index]);
+        Console.WriteLine("Username: " + username[index]);
+        Console.WriteLine("Password: " + password[index]);
+        Console.ReadLine();
+
+    }
+}
+
